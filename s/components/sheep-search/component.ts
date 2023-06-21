9ea4742a433550b321c4cba1a31310b9dc379fa3
Chase Moskal <chasemoskal@gmail.com>
2023-06-20 19:02:58 -0700

    add: proper feather icons

--- s/components/sheep-search/component.ts
+++ s/components/sheep-search/component.ts
@@ -1,9 +1,10 @@
 
-import {html} from "lit"
+import {html, svg} from "lit"
 
 import {style} from "./style.css.js"
 import {Context} from "../context.js"
 import {QuickElement} from "../../framework/quick-element.js"
+import {search_icon} from "../../icons/feather/search_icon.js"
 
 export const SheepSearch = ({router}: Context) => (
 	class extends QuickElement<boolean> {
@@ -34,7 +35,7 @@ export const SheepSearch = ({router}: Context) => (
 		render() {
 			return html`
 				<input type="text" @input="${this.#search}"/>
-				<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-search"><circle cx="11" cy="11" r="8"></circle><line x1="21" y1="21" x2="16.65" y2="16.65"></line></svg>
+				${search_icon(svg)}
 			`
 		}
 	}
--- s/index.html.ts
+++ s/index.html.ts
@@ -1,5 +1,6 @@
 
-import {template, html, easypage, startup_scripts_with_debug_mode} from "@benev/turtle"
+import {shopping_cart_icon} from "./icons/feather/shopping_cart_icon.js"
+import {template, html, easypage, startup_scripts_with_debug_mode, html as svg} from "@benev/turtle"
 
 export default template(async basics => {
 	const path = basics.path(import.meta.url)
@@ -29,20 +30,12 @@ export default template(async basics => {
 				<div class=bigbar>
 
 					<div class=search-area>
-						<div class=search-box>
-							<input type="text"/>
-							<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-search"><circle cx="11" cy="11" r="8"></circle><line x1="21" y1="21" x2="16.65" y2="16.65"></line></svg>
-						</div>
-						<ul class=tags>
-							<li>alpha</li>
-							<li>beta</li>
-						</ul>
 						<sheep-search></sheep-search>
 					</div>
 
 					<div class=cart-area>
 						<button class=cart-button>
-							<svg class=cart-icon xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-shopping-cart"><circle cx="9" cy="21" r="1"></circle><circle cx="20" cy="21" r="1"></circle><path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6"></path></svg>
+							${shopping_cart_icon(svg)}
 						</button>
 						<div class=cartpanel hidden>
 							shopping cart
