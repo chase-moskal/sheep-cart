72e6de59eab44f5b1652f4203043ecdbd83a3c2d
Chase Moskal <chasemoskal@gmail.com>
2023-07-24 11:47:09 -0700

    rename: 'component' files to 'element'

--- s/components/prepare_all_components.ts
+++ s/components/prepare_all_components.ts
@@ -3,10 +3,10 @@ import {Pipe, flatstate_reactivity, apply_theme, requirement} from "@benev/frog"
 
 import {Context} from "../context/context.js"
 
-import {SheepTags} from "./sheep-tags/component.js"
-import {SheepSearch} from "./sheep-search/component.js"
-import {SheepCatalog} from "./sheep-catalog/component.js"
-import {SheepCollections} from "./sheep-collections/component.js"
+import {SheepTags} from "./sheep-tags/element.js"
+import {SheepSearch} from "./sheep-search/element.js"
+import {SheepCatalog} from "./sheep-catalog/element.js"
+import {SheepCollections} from "./sheep-collections/element.js"
 
 export const prepare_all_components = (context: Context) => {
 	return Pipe.with({
