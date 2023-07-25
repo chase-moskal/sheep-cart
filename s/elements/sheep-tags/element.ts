58fa72b47b2fe0a142dc5a5a0033bf1126288d6f
zenkyu <przemekgg2002@gmail.com>
2023-07-25 23:18:07 +0200

    refactor: sheep-tags

--- s/elements/sheep-tags/element.ts
+++ s/elements/sheep-tags/element.ts
@@ -4,52 +4,20 @@ import {QuickElement} from "@benev/frog"
 
 import {style} from "./style.css.js"
 import {Context} from "../../context/context.js"
+import {update_tag} from "./parts/update_tag.js"
+import {get_tag_data} from "./parts/get_tag_data.js"
 
-export const SheepTags = ({router, state}: Context) => class extends QuickElement {
+export const SheepTags = (context: Context) => class extends QuickElement {
 	static styles = style
 
 	render() {
-		const {route, tags} = state
-		const active_tag_names = (route && route.zone === "search")
-			? route.tags
-			: []
-
-		function is_tag_active(tag: string) {
-			return active_tag_names.includes(tag)
-		}
-
-		function update_tag(tag: string) {
-			return (event: InputEvent) => {
-				const target = event.target as HTMLInputElement
-				const terms = router.search_terms
-				const tag_set = new Set<string>(router.search_tags)
-
-				if (target.checked)
-					tag_set.add(tag)
-				else
-					tag_set.delete(tag)
-
-				const nada = tag_set.size === 0 && terms.length === 0
-
-				if (nada)
-					router.routes.catalog().go()
-				else
-					router.routes.search(terms, [...tag_set]).go()
-			}
-		}
-
-		const tag_data = tags.map(tag => ({
-			tag,
-			active: is_tag_active(tag),
-		}))
-
 		return html`
-			${tag_data.map(({tag, active}) => html`
+			${get_tag_data(context).map(({tag, active}) => html`
 				<label ?data-active="${active}">
 					<input
 						type="checkbox"
 						.checked="${active}"
-						@input="${update_tag(tag)}"
+						@input="${update_tag(tag, context.router)}"
 						/>
 					<span>${tag}</span>
 				</label>
