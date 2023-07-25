4b74e47d7051ad92af34d96b6f16d54765e12d44
Chase Moskal <chasemoskal@gmail.com>
2023-07-25 13:40:28 -0700

    refactor: search element

--- s/elements/sheep-search/element.ts
+++ s/elements/sheep-search/element.ts
@@ -6,16 +6,29 @@ import {debounce} from "@chasemoskal/magical"
 import {style} from "./style.css.js"
 import {Context} from "../../context/context.js"
 import {search_icon} from "../../icons/feather/search_icon.js"
+import {ascertain_search_details} from "./parts/ascertain_search_details.js"
+import {populate_input_with_route_search_terms} from "./parts/populate_input_with_route_search_terms.js"
+import {populate_input_with_route_search_terms_when_user_is_not_focused_on_input} from "./parts/populate_input_with_route_search_terms_when_user_is_not_focused_on_input.js"
 
 export const SheepSearch = ({router}: Context) => class extends QuickElement {
 	static styles = style
 
 	constructor() {
 		super()
-		this.wait.then(() => {
-			if (router.route.zone === "search")
-				this.#input.value = router.route.terms.join(" ")
-		})
+
+		populate_input_with_route_search_terms(
+			this.wait,
+			router,
+			() => this.#input,
+		)
+
+		this.add_setup(() => router.on_route_change(route =>
+			populate_input_with_route_search_terms_when_user_is_not_focused_on_input(
+				route,
+				this.#user_is_focused_on_input,
+				this.#input,
+			)
+		))
 	}
 
 	get #input() {
@@ -26,40 +39,13 @@ export const SheepSearch = ({router}: Context) => class extends QuickElement {
 		return document.activeElement === this || this.contains(document.activeElement)
 	}
 
-	#unsub_from_route_change: (() => void) | undefined
-
-	connectedCallback() {
-		super.connectedCallback()
-
-		this.#unsub_from_route_change = router.on_route_change(route => {
-			const is_search = route.zone === "search"
-			const not_focused = !this.#user_is_focused_on_input
-
-			if (not_focused) {
-				if (is_search)
-					this.#input.value = route.terms.join(" ")
-				else
-					this.#input.value = ""
-			}
-		})
-	}
-
-	disconnectedCallback() {
-		super.disconnectedCallback()
-		if (this.#unsub_from_route_change)
-			this.#unsub_from_route_change()
-	}
-
 	#search: () => Promise<void> = debounce(250, () => {
-		const {value} = this.#input
-		const terms = value.split(/\s+/).filter(t => t.length)
-		const tags = router.search_tags
-		const nada = terms.length === 0 && tags.length === 0
+		const details = ascertain_search_details(router, this.#input.value)
 
-		if (nada)
+		if (details.there_is_nothing_to_search_for)
 			router.routes.catalog().go()
 		else
-			router.routes.search(terms, router.search_tags).go()
+			router.routes.search(details.terms, router.search_tags).go()
 	})
 
 	render() {
