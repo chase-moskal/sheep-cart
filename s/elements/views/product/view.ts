776b1ca713be2c7f4c0a4610a8249a7c95f7bb54
zenkyu <przemekgg2002@gmail.com>
2023-07-25 02:39:17 +0200

    sketch: product view

--- s/elements/views/product/view.ts
+++ s/elements/views/product/view.ts
@@ -1,7 +1,7 @@
 
 import {css, html} from "lit"
 import {flatview} from "@benev/frog"
-import {GqlProduct} from "shopify-shepherd"
+import {edges, GqlProduct, GqlVariant} from "shopify-shepherd"
 import {unsafeHTML} from "lit/directives/unsafe-html.js"
 
 import {Context} from "../../../context/context.js"
@@ -18,21 +18,43 @@ export const Product = (context: Context) => flatview(context.flat)
 
 	.setup(({actions}) => {
 		const interval = setInterval(() => {
-			actions.increment()
+			// actions.increment()
 		}, 1000)
 		return () => clearInterval(interval)
 	})
 
-	.render(({state, actions}) => (product: GqlProduct) => html`
-		<div class=product>
-			<p>${product.title}<p>
-			<ul>${product.tags.map(tag => html`<li>${tag}</li>`)}</ul>
-			<div>${unsafeHTML(product.descriptionHtml)}</div>
-			<p>count: ${state.count}</p>
-			<button @click=${actions.increment}>increment</button>
-			<a href="https://github.com/" part="a">github.com</a>
-		</div>
-	`)
+	.render(({state, actions}) => (product: GqlProduct) => {
+
+		const imageEdge = product.images.edges
+			.find(n => n.node.id === product.featuredImage?.id)
+		const more_than_one_variant = product.variants.edges.length > 1
+		const all_the_same_price = product.variants.edges
+			.every(p => p.node.price.amount === product.variants.edges[0].node.price.amount)
+		function price_for({node: variant}: {node: GqlVariant}) {
+			return parseFloat(variant.price.amount)
+		}
+		const lowest_price = product.variants.edges.reduce(
+			(previous, current) => (price_for(current) < previous)
+				? price_for(current)
+				: previous,
+			price_for(product.variants.edges[0])
+		)
+
+		return html`
+			<div class=product>
+				<img src="${imageEdge!.node.url_tiny}"/>
+				<div>
+					<p>${product.title}</p>
+					<ul>${product.tags.map(tag => html`<li>${tag}</li>`)}</ul>
+					<div>${unsafeHTML(product.descriptionHtml)}</div>
+					<p>variants: ${product.variants.edges.length}</p>
+					${more_than_one_variant && !all_the_same_price
+						? html`starts at ${lowest_price}`
+						: html`${lowest_price}`}
+					<button>ADD TO CART</button>
+				</div>
+			</div>
+		`})
 
 	.css(context.theme, css`
 		.product {
