0512aa33cf0a5b85e710fc0bd010434570c7723e
Chase Moskal <chasemoskal@gmail.com>
2023-07-28 01:33:21 -0700

    tweak: syntax

--- s/elements/views/product_focus/view.ts
+++ s/elements/views/product_focus/view.ts
@@ -6,17 +6,20 @@ import {unsafeHTML} from "lit/directives/unsafe-html.js"
 import {style} from "./style.css.js"
 import {flappy} from "../../flappy.js"
 import {Viewbase} from "../../viewbase.js"
-import {Choice, get_primary_img, render_img, render_options, render_side_images, number_of_images, number_of_variants, first_variant} from "./parts/sketch.js"
+import {Choice, get_primary_img, render_img, render_options, render_side_images, number_of_images, number_of_variants, get_selected_variant} from "./parts/sketch.js"
 
 export const ProductFocus = flappy("article", "product-focus")
 	.render(context => use => (product: GqlProduct) => {
+		const {views} = context
 
 		const state = use.state({
 			choices: [] as Choice[],
 		})
 
 		function set_choice(name: string, value: undefined | string) {
-			state.choices = state.choices.filter(choice => choice.name !== name)
+			state.choices = state.choices
+				.filter(choice => choice.name !== name)
+
 			if (value !== undefined) {
 				state.choices.push({name, value})
 			}
@@ -34,15 +37,17 @@ export const ProductFocus = flappy("article", "product-focus")
 
 				<h1>${product.title}</h1>
 
-				${context.views.Pills({class: "pills"})(product)}
+				${views.Pills({class: "pills"})(product)}
 
 				<div class=options>
 					${render_options(product, state.choices, set_choice)}
 				</div>
 
 				<div class=buy>
-					${context.views.Price({class: "price"})(first_variant(product).price)}
-					${context.views.Coolbutton()("Add to Cart", () => {})}
+					${views.Price({class: "price"})(
+						get_selected_variant(product, state.choices).price
+					)}
+					${views.Coolbutton()("Add to Cart", () => {})}
 				</div>
 
 				<aside>
