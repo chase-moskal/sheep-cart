a6ba0fd46f31776422e345c82c566a9d2da1d2e0
Chase Moskal <chasemoskal@gmail.com>
2023-07-29 21:49:57 -0700

    fix: product_recommendation dir name

--- s/elements/prepare_all_views.ts
+++ s/elements/prepare_all_views.ts
@@ -10,7 +10,7 @@ import {ProductList} from "./views/product_list/view.js"
 import {ProductCard} from "./views/product_card/view.js"
 import {ProductFocus} from "./views/product_focus/view.js"
 import {CollectionList} from "./views/collection_list/view.js"
-import {ProductRecommendation} from "./views/product-recommendation/view.js"
+import {ProductRecommendation} from "./views/product_recommendation/view.js"
 
 export function prepare_all_views(context: Context) {
 	return Pipe.with({
@@ -21,7 +21,7 @@ export function prepare_all_views(context: Context) {
 			ProductCard,
 			ProductFocus,
 			CollectionList,
-			ProductRecommendation
+			ProductRecommendation,
 		})
 		.to(requirement.provide(context))
 		.done()
