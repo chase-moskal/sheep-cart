6994b669e828bcdd40526c2f6571734a438b95bf
zenkyu <przemekgg2002@gmail.com>
2023-08-01 02:01:12 +0200

    fix: modal dialog undefined img

--- s/elements/sheep-modal/element.ts
+++ s/elements/sheep-modal/element.ts
@@ -6,13 +6,13 @@ import {Context} from "../../context/context.js"
 
 export const SheepModal = (context: Context) => class extends QuickElement {
 	static styles = style
-
+	
 	render() {
 		return html`
 			<dialog
 				part="dialog"
 				@click=${context.modal.close}
-				?open=${!!context.modal.modal_state}>
+				?open=${!!context.modal.modal_state.img}>
 				<img
 					@click=${context.modal.close}
 					alt=${context.modal.modal_state.img?.alt!}
