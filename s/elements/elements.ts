8cacf3d763fbc3aefe25c22bb4c0cac4c15ec753
Chase Moskal <chasemoskal@gmail.com>
2023-08-26 15:31:42 -0700

    delete: obsolete sheep-cart-modal

--- s/elements/elements.ts
+++ s/elements/elements.ts
@@ -6,7 +6,6 @@ import {SheepCart} from "./sheep-cart/element.js"
 import {SheepModal} from "./sheep-modal/element.js"
 import {SheepSearch} from "./sheep-search/element.js"
 import {SheepCatalog} from "./sheep-catalog/element.js"
-import {SheepCartModal} from "./sheep-cart-modal/element.js"
 import {SheepCartButton} from "./sheep-cart-button/element.js"
 
 export const elements = components({
@@ -15,7 +14,6 @@ export const elements = components({
 	SheepModal,
 	SheepSearch,
 	SheepCatalog,
-	SheepCartModal,
 	SheepCartButton,
 })
 
