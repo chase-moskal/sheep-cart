957460f638413e5440c8ad9951b4d86b7c7d5eaa
Chase Moskal <chasemoskal@gmail.com>
2023-08-28 16:07:53 -0700

    tweak: bg_img to url_small for collections

--- s/elements/views/collection_list/utils/bg_img.ts
+++ s/elements/views/collection_list/utils/bg_img.ts
@@ -3,7 +3,7 @@ import {GqlCollection} from "shopify-shepherd"
 
 export function bg_img(collection: GqlCollection) {
 	return collection.image
-		? `background-image: url("${collection.image.url_tiny}");`
+		? `background-image: url("${collection.image.url_small}");`
 		: ""
 }
 
