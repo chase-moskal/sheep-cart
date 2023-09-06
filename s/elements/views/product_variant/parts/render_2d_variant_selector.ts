4824d4cde3b97d7ea6f25a304c1ddab60367d9f9
Paul_Aroo <timmypaul2006@gmail.com>
2023-09-06 01:50:56 +0100

    remove: commented code in 2d variant selector

--- s/elements/views/product_variant/parts/render_2d_variant_selector.ts
+++ s/elements/views/product_variant/parts/render_2d_variant_selector.ts
@@ -3,8 +3,8 @@ import {html, svg} from "lit"
 
 import {VariantSelectorOptions} from "./types.js"
 
-import icon_check from "../../../../icons/feather/icon_check.js"
 import icon_x from "../../../../icons/feather/icon_x.js"
+import icon_check from "../../../../icons/feather/icon_check.js"
 
 export function render_2d_variant_selector({
 		cart, set_variant, product_helper, selected_variant
@@ -71,15 +71,13 @@ export function render_2d_variant_selector({
 										@click=${() => set_variant(v)}
 										?data-selected=${is_selected(v.id)}
 										?data-in-cart=${status === "in cart"}
-										?data-sold-out=${status === "sold out"}>
-										<!-- ${!!status ? html`<p class=status>${status}</p>` : undefined} -->
+										?data-sold-out=${status === "sold out"}>-->
 										<div class=img>
 											<img
 												alt="${img.altText}"
 												src="${img.url_tiny}"/>
 											${icon}
 										</div>
-										<!-- <p>${v.title.split("/")[1]}</p> -->
 									</div>
 								`
 							})}
@@ -89,61 +87,4 @@ export function render_2d_variant_selector({
 			</div>
 		</div>
 	`
-
-	// return html`
-	// 	<div class=dd>
-	// 		<div>
-	// 			${values_for_second_option.map(v => (
-	// 				html`<p>${v}</p>`
-	// 			))}
-	// 		</div>
-	// 		${y.map((d) => (
-	// 			html`
-	// 				<div class="d-y">
-	// 					<p>${d.value}</p>
-	// 					<div class=thmb>
-	// 						${d.variants.map(v => {
-	// 							const img = product_helper.get_variant_image(v.id) ?? {
-	// 								altText: `${v.title} image`,
-	// 								url_tiny: "https://i.imgur.com/h1v2noQ.webp"
-	// 							}
-	// 							const unit = cart.units
-	// 								.find(u => u.variant_id === v.id)
-				
-	// 							const status = unit
-	// 								? "in cart"
-	// 								: v.availableForSale
-	// 									? ""
-	// 									: "sold out"
-				
-	// 							const icon = status === "in cart"
-	// 								? html`<div class=icon>${icon_check(svg)}</div>`
-	// 								: status === "sold out"
-	// 									? html`<div class=icon>${icon_x(svg)}</div>`
-	// 									: undefined
-				
-	// 							return html`
-	// 								<div
-	// 									class=thumbnail
-	// 									@click=${() => set_variant(v)}
-	// 									?data-selected=${is_selected(v.id)}
-	// 									?data-in-cart=${status === "in cart"}
-	// 									?data-sold-out=${status === "sold out"}>
-	// 									<!-- ${!!status ? html`<p class=status>${status}</p>` : undefined} -->
-	// 									<div class=img>
-	// 										<img
-	// 											alt="${img.altText}"
-	// 											src="${img.url_tiny}"/>
-	// 										${icon}
-	// 									</div>
-	// 									<!-- <p>${v.title.split("/")[1]}</p> -->
-	// 								</div>
-	// 							`
-	// 						})}
-	// 					</div>
-	// 				</div>
-	// 			`
-	// 		))}
-	// 	</div>
-	// `
 }
